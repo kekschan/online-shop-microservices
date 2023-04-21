b42b43c7ff5363c5cdac3b28235a46de13bee1f6
Denis Borshev <dnsborshev@gmail.com>
2023-04-21 12:05:20 +0300

    добавили микросервис product-service

---
+++ product-service/src/test/java/ru/dnsbo/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dnsbo.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
