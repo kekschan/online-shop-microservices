f5b117e5c6346b115aa5f096077d4f486addd265
Denis Borshev <dnsborshev@gmail.com>
2023-04-21 15:52:42 +0300

    добавили микросервис сервис-хранилища

---
+++ inventory-service/src/test/java/ru/dnsbo/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dnsbo.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
